c8d8998999841102b3c575df7ef8c683c026f990
Raj Chaudhary <craj68168@gmail.com>
2023-08-17 13:35:39 +0545

    feat:handlers creting

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,24 +1,30 @@
 package db
 
 import (
+	"context"
 	"sync"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var clientInstance *mongo.Client
 
 var mongoOnce sync.Once
+var clientInstanceError error
 
 const (
 	url      = "mongodb://localhost:27017"
 	Database = "products-api"
 )
 
-// func GetMongoClient() (*mongo.Client, error) {
-// 	mongoOnce.Do(func() {
-// 		clientOptions := options.Client().ApplyURI(url) //options comes from mongo driver options
+func GetMongoClient() (*mongo.Client, error) {
+	mongoOnce.Do(func() {
+		clientOptions := options.Client().ApplyURI(url) //options comes from mongo driver options
 
-// 		client, err := mongo.Connect(context.TODO(), client.Options)
-// 	})
-// }
+		client, err := mongo.Connect(context.TODO(), clientOptions)
+		clientInstance = client
+		clientInstanceError = err
+	})
+	return clientInstance, clientInstanceError
+}
