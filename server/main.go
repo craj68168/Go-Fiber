07e9ccee8c7bf0aec721675b2c04a206098dd568
Raj Chaudhary <craj68168@gmail.com>
2023-09-11 16:34:02 +0545

    feat:post api created and checked

--- server/main.go
+++ server/main.go
@@ -7,13 +7,34 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID    int    `json:"id"`
+	TITLE string `json:"title"`
+	DONE  bool   `json:"done"`
+	BODY  string `json:"body"`
+}
+
 func main() {
 	fmt.Print("Hello World")
 
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if error := c.BodyParser(todo); error != nil {
+			return error
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
 	})
 
 	log.Fatal(app.Listen(":4000"))
