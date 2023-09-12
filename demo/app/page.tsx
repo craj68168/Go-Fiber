edc4984d438b73f761dcc1fa4cee53c3319bc79a
Raj Chaudhary <craj68168@gmail.com>
2023-09-12 13:49:41 +0545

    feat:added to the backend

--- demo/app/page.tsx
+++ demo/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { Box } from "@mantine/core";
+import AddTodos from "@/components/AddTodos";
+import { Box, List, ThemeIcon } from "@mantine/core";
+import { CheckCircleFillIcon } from "@primer/octicons-react";
 import useSWR from "swr";
 
 export const END_POINT = "http://localhost:4000";
@@ -7,10 +9,41 @@ const fetcher = (url: any) => {
   return fetch(`${END_POINT}/${url}`).then((r) => r.json());
 };
 export default function Home() {
-  const { data } = useSWR("api/todos", fetcher);
+  const { data, mutate } = useSWR("api/todos", fetcher);
+  console.log("data", data);
+
   return (
     <>
-      <Box>{JSON.stringify(data)}</Box>
+      <Box
+        sx={() => ({
+          padding: "2rem",
+          width: "100%",
+          maxWidth: "40rem",
+          margin: "0 auto",
+        })}
+      >
+        <List center spacing={"xs"} size="sm" mb={12}>
+          {data?.map((d: any) => {
+            return (
+              <List.Item
+                key={d?.id}
+                icon={
+                  d.done ? (
+                    <ThemeIcon color={"teal"} radius="xl" size={24}>
+                      <CheckCircleFillIcon size={20}/>
+                    </ThemeIcon>
+                  ) : ( <ThemeIcon color={"gray"} radius="xl" size={24}>
+                  <CheckCircleFillIcon size={20}/>
+                </ThemeIcon>)
+                }
+              >
+                {d?.title}
+              </List.Item>
+            );
+          })}
+        </List>
+      </Box>
+      <AddTodos mutate={mutate} />
     </>
   );
 }
---
+++ demo/components/AddTodos.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useState } from "react";
+import { Button, Group, Modal, Textarea, TextInput } from "@mantine/core";
+import { useFormik } from "formik";
+export const END_POINT = "http://localhost:4000";
+const AddTodos = ({ mutate }: any) => {
+  const [open, setOpen] = useState(false);
+  const initialValues = {
+    title: "",
+    body: "",
+  };
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    onSubmit: (values) => {
+      createTodo(values);
+    },
+  });
+  const createTodo = async (values: any) => {
+    const update = await fetch(`${END_POINT}/api/todos`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    }).then((r) => r.json());
+
+    mutate(update);
+    formik.resetForm();
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Modal opened={open} onClose={() => setOpen(false)} title="Create todo">
+        <form onSubmit={formik.handleSubmit}>
+          <TextInput
+            mb={12}
+            required
+            label={"Todo"}
+            placeholder="Please enter"
+            onChange={formik.handleChange}
+            name="title"
+            value={formik.values.title}
+          />
+          <Textarea
+            mb={12}
+            required
+            label={"Body"}
+            placeholder="Please enter"
+            onChange={formik.handleChange}
+            name="body"
+            value={formik.values.body}
+          />
+          <Button type="submit">Create Todo</Button>
+        </form>
+      </Modal>
+      <Group position="center">
+        <Button fullWidth mb={12} onClick={() => setOpen(true)}>
+          Add Todo
+        </Button>
+      </Group>
+    </div>
+  );
+};
+
+export default AddTodos;
