8d826ab397e23dc9950e04a1bd21914b17f765c6
Raj Chaudhary <craj68168@gmail.com>
2023-09-12 12:06:16 +0545

    feat:cors error resolve

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Todo struct {
@@ -19,9 +20,14 @@ func main() {
 
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	todos := []Todo{}
 
-	app.Get("healthcheck", func(c *fiber.Ctx) error {
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
 		return c.JSON(todos)
 	})
 
